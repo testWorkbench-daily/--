484529cfe32bf0b961d7b0c3dc6eaa39e233b28a
杨林 <929698892@qq.com~>
2022-01-12 14:51:04 +0800

    new

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 xcode-select --install
 export SDKROOT=$(xcrun --show-sdk-path)
+Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+Install Ruby
+brew install ruby
 cnpm install
 cnpm install jekyll bundler
 bundler exec jekyll build
